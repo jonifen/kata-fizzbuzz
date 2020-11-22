0d9a6e6676bae24c2fede56fd25f723331b04e13
Jon Cain <jonifen@users.noreply.github.com>
2020-11-22 12:49:27 +0000

    First attempt in JS

---
+++ src/fizzbuzz.js
@@ -0,0 +1,26 @@
+function calculateFizzBuzz(iterations) {
+  let output = [];
+
+  for (let iteration = 0; iteration < iterations; iteration++) {
+    output.push(calculateIteration(iteration));
+  }
+
+  return output;
+}
+
+function calculateIteration(value) {
+  if (value > 0 && value % 3 == 0 && value % 5 == 0)
+    return "fizz buzz";
+
+  if (value > 0 && value % 3 == 0)
+    return "fizz";
+
+  if (value > 0 && value % 5 == 0)
+    return "buzz";
+
+  return value;
+}
+
+export {
+  calculateFizzBuzz
+};
